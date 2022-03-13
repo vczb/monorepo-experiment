972da85cc2dc3c8daa701bba72ab979bcfc10307
Vinicius Zucatti <viniciuszucatti@gmail.com>
2022-03-13 12:25:06 -0300

    Add support to Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { Button, createTheme, ThemeProvider } from "@mui/material";
+
+import { orange } from "@mui/material/colors";
+import { colors } from "theme/colors";
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      light: orange[300],
+      main: orange[700],
+      dark: orange[800],
+      contrastText: colors.white,
+    },
+  },
+});
 
 function App() {
-  return <>Hello</>
+  return (
+    <ThemeProvider theme={theme}>
+      <Button variant="contained">Hello</Button>
+    </ThemeProvider>
+  );
 }
 
 export default App;
