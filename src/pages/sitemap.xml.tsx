1c5d406bd4331c942e97ea206bdbcb56af9198f6
Vinicius Zucatti <viniciuszucatti@gmail.com>
2022-12-14 19:43:48 -0300

    Add sitemap.xml

---
+++ src/pages/sitemap.xml.tsx
@@ -0,0 +1,32 @@
+const Sitemap = () => {};
+
+export const getServerSideProps = ({ res }: any) => {
+  const staticPages = [process.env.NEXT_PUBLIC_BASE_URL];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${staticPages
+        .map((url) => {
+          return `
+            <url>
+              <loc>${url}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>monthly</changefreq>
+              <priority>1.0</priority>
+            </url>
+          `;
+        })
+        .join("")}
+    </urlset>
+  `;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap;
